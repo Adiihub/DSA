b9980c40765a5ce9d3597c924ae58bc8adff9818
Aditi Kumari <aditi22202@gmail.com>
2025-01-13 11:28:54 +0530

    Recursion practice

---
+++ Lecture 16 - Recursion/6Permutation/3Permutation2LC.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    void permu(vector<int>& arr, int index, set<vector<int>>& ans) {
+        if (index == arr.size()) {
+            ans.insert(arr); // Insert the permutation into the set
+            return;
+        }
+        for (int i = index; i < arr.size(); i++) {
+            swap(arr[index], arr[i]); // Swap current element with index
+            permu(arr, index + 1, ans); // Recurse for the next index
+            swap(arr[index], arr[i]); // Backtrack to restore original state
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        set<vector<int>> ans; // To store unique permutations
+        permu(nums, 0, ans);
+        vector<vector<int>> res(ans.begin(), ans.end()); // Convert set to vector
+        return res;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Example input
+    vector<int> nums = {1, 1, 2};
+
+    // Get unique permutations
+    vector<vector<int>> result = solution.permuteUnique(nums);
+
+    // Print the results
+    cout << "Unique Permutations:" << endl;
+    for (const auto& perm : result) {
+        for (int num : perm) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Lecture 16 - Recursion/6Permutation/4WaysToSumToN_GFG.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+ 
+class Solution
+{
+  public:
+    // function to count the total  
+    // number of ways to sum up to 'N' 
+    int Ways(int arr[], int n, int sum){
+        if(sum == 0)
+        return 1;
+        if(sum < 0)
+        return 0;
+        long long ans =0;
+        for(int i=0; i<n; i++){
+            ans += Ways(arr, n, sum-arr[i]);
+        }
+        return ans%1000000007;
+    }
+    int countWays(int arr[], int m, int N) 
+    { 
+      return Ways(arr, m, N);
+    } 
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int m,n ;
+	        cin>>m>>n;
+	        int arr[m];
+	        for(int i = 0;i<m;i++)
+	            cin>>arr[i];
+	        Solution ob;     
+	        cout<<ob.countWays(arr,m,n)<<endl;
+	    
+cout << "~" << "\n";
+}
+} 
