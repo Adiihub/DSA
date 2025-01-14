021d86ae21efca893c297e037e53fe58538392e5
Aditi Kumari <aditi22202@gmail.com>
2025-01-14 21:54:29 +0530

    --

--- 03 - Recursion/HardProblelms/tower_of_hanoi.cpp
+++ 03 - Recursion/HardProblelms/tower_of_hanoi.cpp
@@ -1,19 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void toh(int n,char src,char help,char des){
+void toh(int n,char src,char help,char des,int &c){
     if(n==0){
         return ;
     }
-    toh(n-1,src,des,help);
+    toh(n-1,src,des,help, c);
     cout<<n<<": "<<src<<"-->"<<des<<endl;
-    toh(n-1,help,src ,des);
+    c++;
+    toh(n-1,help,src ,des, c);
 }
 
 int main(){
     int n;
     cin>>n;
-    toh(n,'A','B','C');
+    int c = 0;
+    toh(n,'A','B','C', c);
+    cout << "Total moves: " << c << endl;
     return 0;
 
 }
