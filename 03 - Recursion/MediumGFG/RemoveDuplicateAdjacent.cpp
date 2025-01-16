a25ab6ef762c4d3c33834905c8b70f7bfbb7da67
Aditi Kumari <aditi22202@gmail.com>
2025-01-17 00:08:09 +0530

    recursion

---
+++ 03 - Recursion/MediumGFG/RemoveDuplicateAdjacent.cpp
@@ -0,0 +1,67 @@
+// Given a string s, remove all its adjacent duplicate characters recursively, until there are no adjacent duplicate characters left.
+
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    string solve(string &s){
+        
+        if(s.length() <= 0){
+            return s;
+        }
+        
+        string result = "";
+        
+        for(int i=0; i<s.length(); i++){
+            if(i < s.length()-1 && s[i] == s[i+1]){
+                while(i < s.length()-1 && s[i] == s[i+1]){
+                    i++;
+                }
+            }
+            else{
+                result += s[i];
+            }
+        }
+        if(result == s){
+            return s;
+        }
+        
+        return solve(result);
+    }
+    string removeUtil(string &s) {
+        return solve(s);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Solution ob;
+        s = ob.removeUtil(s);
+        if (s.size() == 0)
+            cout << "\"\"";
+        else
+            cout << s << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
