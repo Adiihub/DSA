3e81e19f9a644e3c9cb7ccb89dd963ce8ce5a657
Aditi Kumari <aditi22202@gmail.com>
2025-02-27 11:08:50 +0530

    strings

--- 03 - Recursion/4Strings/String_to_Interger.cpp
+++ 03 - Recursion/4Strings/String_to_Interger.cpp
@@ -4,14 +4,24 @@ using namespace std;
 int string_to_int(string s,int n){
     if(n==0)
         return 0;
-    int ld=s[n]-'0';
+    int ld=s[n-1]-'0';
     //int CA=string_to_int(s,n-1);
     return string_to_int(s,n-1)*10+ld;
 }
+
+void converSTInt(string s, int i, int n, int &res){
+    if(i == n){
+        return;
+    }
+    res = res * 10 + s[i] - '0';
+    converSTInt(s, i+1, n,  res);
+}
+
 int main(){
     string s="1023";
     cin>>s;
     int n=s.size();
-    cout<<string_to_int(s,n-1)<<endl;
+    int res = 0;
+    cout<<string_to_int(s,n)<<endl;
     return 0;
 }
